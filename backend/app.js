f3506f5a7059e20f93e1c1a4bc8f05d1e7ace746
jjyoon7 <jay.yoon7@gmail.com>
2020-06-23 20:22:17 +0200

    delete routes, since graphQL does not need it

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const multer = require('multer')
 
-const feedRoutes = require('./routes/feed')
-const authRoutes = require('./routes/auth')
-
 const app = express()
 
 const morgan = require('morgan')
@@ -59,8 +56,7 @@ app.use(cors({
 
 app.use(morgan('dev'))
 
-app.use('/feed', feedRoutes)
-app.use('/auth', authRoutes)
+
 
 app.use((error, req, res, next) => {
     console.log(error)
@@ -81,10 +77,6 @@ mongoose.connect(uri, {
         .then(result => {
             console.log(`server is running on port ${PORT}`)
             console.log('mongoDB database connection established successfully.')
-            const server = app.listen(PORT)
-            const io = require('./socket').init(server)
-            io.on('connection', socket => {
-                console.log('Client connected')
-            })
+            app.listen(PORT)
         })
         .catch(err => console.log(err))
