cfd65af92943217c40a766dc75902a71d81e425f
jjyoon7 <jay.yoon7@gmail.com>
2020-06-24 20:32:10 +0200

    in graphQL, user input validation happens in 'resolvers', in REST api, it would happen in routes

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,9 +1,25 @@
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 const User = require('../models/user')
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
-        
+        const error = []
+
+        if(!validator.isEmail(userInput.email)) {
+            error.push({ message: 'Email invalid' })
+        }
+        if(
+            validator.isEmpty(userInput.password) ||
+            !validator.isLength({ min: 5 })
+        ) {
+            error.push({ message: 'Password too short' })
+        }
+        if(errors.length > 0) {
+            const error = new Error('Invalid input')
+            throw error
+        }
+
         const existingUser = await User.findOne({email: userInput.email})
         
         if(existingUser) {
