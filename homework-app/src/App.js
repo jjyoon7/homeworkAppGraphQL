3c31e8b9ceeff7967c2a5a20a8cdb6a97c8ba64e
jjyoon7 <jay.yoon7@gmail.com>
2020-06-24 21:02:13 +0200

    in frontend, now the fetch url endpoint is always '/graphql', and the method is 'POST'. Also what you pass as body is 'JSON.stringify(graphqlQuery)'. Note that the graphqlQuery objects key value pair - value should be in double quotation mark.

--- homework-app/src/App.js
+++ homework-app/src/App.js
@@ -110,16 +110,26 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('http://localhost:5000/auth/signup', {
-      method: 'PUT',
+    const graphqlQuery = {
+      query: `
+          mutation {
+            createUser(userInput: { 
+                                    email: "${authData.signupForm.email.value}" 
+                                    name: "${authData.signupForm.name.value}"
+                                    password: "${authData.signupForm.password.value}"
+                                  }) {
+                                    _id
+                                    name
+                                  }
+          }
+      `
+    }
+    fetch('http://localhost:5000/graphql', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: authData.signupForm.email.value,
-        password: authData.signupForm.password.value,
-        name: authData.signupForm.name.value
-      })
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
         if (res.status === 422) {
