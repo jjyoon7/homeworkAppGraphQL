5117f00df61402381c11f6eeb7f712d208139bf3
jjyoon7 <jay.yoon7@gmail.com>
2020-06-24 19:10:10 +0200

    note that graphQl does not know date, that is why createAt value type is String. Also, array type is '[]' and you can make it required as '[]!'

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -1,8 +1,23 @@
 const { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
     tyepe User {
         _id: ID!
+        email: String!
+        name: String!
+        password: String
+        status: String!
+        posts: [Post!]!
     }
 
     input UserInputData {
