f83e68f377b9dce539350a9c035a223a70b7fa62
jjyoon7 <jay.yoon7@gmail.com>
2020-06-24 19:50:37 +0200

    note that the reason for returning the user object is because in 'schema', the 'createUser(userInput: UserInputData): User!', here the ': User!' means User object is required as returned object

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -3,6 +3,7 @@ const User = require('../models/user')
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
+        
         const existingUser = await User.findOne({email: userInput.email})
         
         if(existingUser) {
