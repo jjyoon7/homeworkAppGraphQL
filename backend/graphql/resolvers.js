66387e86ae648653eb60f33fa2408c8f56e61f34
jjyoon7 <jay.yoon7@gmail.com>
2020-06-24 19:22:17 +0200

    with async/await, it automatically 'return' for you

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
+        
         const existingUser = await User.findOne({email: userInput.email})
     }
 }
