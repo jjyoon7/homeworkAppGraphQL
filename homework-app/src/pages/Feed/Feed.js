cbcbdf0146033b85ae02ccb0861ce1d8ad2ae93d
jjyoon7 <jay.yoon7@gmail.com>
2020-06-27 16:16:01 +0200

    update the 'finishEditHandler' so it creaetes the new post then also load it as well

--- homework-app/src/pages/Feed/Feed.js
+++ homework-app/src/pages/Feed/Feed.js
@@ -194,11 +194,21 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = posts
+          } else {
+            updatedPosts.unshift(post)
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
-          };
+          }
         });
       })
       .catch(err => {
