ff01c15385bd5001be5d772bf41f541f20ad9ca4
jjyoon7 <jay.yoon7@gmail.com>
2020-06-29 19:57:53 +0200

    update dynamic variables in graphqlQuery, to use variables

--- homework-app/src/pages/Feed/Feed.js
+++ homework-app/src/pages/Feed/Feed.js
@@ -69,8 +69,8 @@ class Feed extends Component {
     }
     const graphqlQuery = {
       query: `
-        {
-          posts(page: ${page}) {
+        query FetchPosts($page: Int!) {
+          posts(page: $page) {
             posts {
               _id
               title
@@ -84,7 +84,10 @@ class Feed extends Component {
             totalPosts
           }
         }
-      `
+      `,
+      variables: {
+        page: page
+      }
     }
     fetch('http://localhost:5000/graphql', {
       method: 'POST',
@@ -126,12 +129,15 @@ class Feed extends Component {
     event.preventDefault();
     const graphqlQuery = {
       query: `
-        mutation {
-          updateStatus(status: "${this.state.status}") {
+        mutation UpdateUserStatus($userStatus: String!) {
+          updateStatus(status: $userStatus) {
             status
           }
         }
-      `
+      `,
+      variables: {
+        userStatus: this.state.status
+      }
     }
     fetch('http://localhost:5000/graphql', {
       method: 'POST',
@@ -193,30 +199,31 @@ class Feed extends Component {
         const imageUrl = fileResData.filePath;
         let graphqlQuery = {
           query: `
-          mutation {
-            createPost(postInput: {title: "${postData.title}", content: "${
-            postData.content
-          }", imageUrl: "${imageUrl}"}) {
-              _id
-              title
-              content
-              imageUrl
-              creator {
-                name
+            mutation CreateNewPost($title: String!, $content: String!, $imageUrl: String!) {
+              createPost(postInput: {title: $title, content: $content, imageUrl: $imageUrl}) {
+                _id
+                title
+                content
+                imageUrl
+                creator {
+                  name
+                }
+                createdAt
               }
-              createdAt
             }
+          `,
+          variables: {
+            title: postData.title,
+            content: postData.content,
+            imageUrl: postData.imageUrl
           }
-        `
         };
 
         if (this.state.editPost) {
           graphqlQuery = {
             query: `
-              mutation {
-                updatePost(id: "${this.state.editPost._id}", postInput: {title: "${postData.title}", content: "${
-                postData.content
-              }", imageUrl: "${imageUrl}"}) {
+              mutation UpdatePost($id: String!, $title: String!, $content: String!, $imageUrl: String!){
+                updatePost(id: $id, postInput: {title: $title, content: $content, imageUrl: $imageUrl) {
                   _id
                   title
                   content
@@ -227,7 +234,13 @@ class Feed extends Component {
                   createdAt
                 }
               }
-            `
+            `,
+            variables: {
+              id: this.state.editPost._id,
+              title: postData.title,
+              content: postData.content,
+              imageUrl: postData.imageUrl
+            }
           };
         }
 
@@ -302,10 +315,13 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     const graphqlQuery = {
       query: `
-          mutation {
-            deletePost(id: "${postId}")
+          mutation DeletePost($id: String!){
+            deletePost(id: $id)
           }
-      `
+      `,
+      varialbes: {
+        id: postId
+      }
     }
     fetch('http://localhost:5000/graphql', {
       method: 'POST',
